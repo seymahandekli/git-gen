abd3d5ca364e14a942b67e1bd146ecadac63022f
Şeyma Handekli <handekliseyma@gmail.com>
2024-07-16 11:37:08 +0300

    Added OpenAI API Integration and HTTP Client to CLI
    - Included the Resty HTTP client library (v2.13.1) in the go.mod file for convenient HTTP requests.
    - Added indirect dependency on golang.org/x/net

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module mycli
 
 go 1.22.3
+
+require github.com/go-resty/resty/v2 v2.13.1
+
+require golang.org/x/net v0.25.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const (
+	apiEndpoint = "https://api.openai.com/v1/chat/completions"
 )
 
 func runDiff() (error, string, string) {
@@ -41,4 +50,39 @@ func main() {
 
 	// Print the output
 	fmt.Println(result)
+
+	apiKey, apiOk := os.LookupEnv("OPENAI_API_KEY")
+	if !apiOk {
+		log.Fatal("Environment variable OPENAI_API_KEY must be set")
+		return
+	}
+
+	client := resty.New()
+
+	response, err := client.R().
+		SetAuthToken(apiKey).
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"model":      "gpt-4o",
+			"messages":   []interface{}{map[string]interface{}{"role": "system", "content": result}},
+			"max_tokens": 50,
+		}).
+		Post(apiEndpoint)
+
+	if err != nil {
+		log.Fatalf("Error while sending send the request: %v", err)
+	}
+
+	body := response.Body()
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error while decoding JSON response:", err)
+		return
+	}
+
+	// Extract the content from the JSON response
+	content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+	fmt.Println(content)
 }
