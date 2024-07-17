2e9f432cea00c54c241747259d333885a4469e89
Şeyma Handekli <handekliseyma@gmail.com>
2024-07-17 19:16:49 +0300

    urfave/cli added

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module mycli
 
 go 1.22.3
 
-require github.com/go-resty/resty/v2 v2.13.1
+require github.com/urfave/cli v1.22.15
 
-require golang.org/x/net v0.25.0 // indirect
+require github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v3 v3.0.0-alpha9
+	golang.org/x/net v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 
-	"github.com/go-resty/resty/v2"
+	"github.com/urfave/cli/v3"
 )
 
 const (
 	apiEndpoint = "https://api.openai.com/v1/chat/completions"
+)
+
+type PromptType int
+
+const (
+	PromptCommitMessage PromptType = iota
+	PromptCodeReview
 )
 
 func runDiff() (error, string, string) {
@@ -35,7 +44,48 @@ func runDiff() (error, string, string) {
 	return nil, stdout.String(), stderr.String()
 }
 
+func getPrompt(promptType PromptType) string {
+	var prompt string
+	switch promptType {
+	case PromptCommitMessage:
+		prompt = "please generate a git commit message with a simple explanation from the changes stated above which is an output of a git diff command. all response of this message should be wrapped in a markdown format because it will be shared in a text-only terminal interface."
+
+	case PromptCodeReview:
+		prompt = "please perform a efficient and concise code review which points out crucial improvements could be changed on the target code. the target code is stated above which is an output of a git diff command. all response of this message should be wrapped in a markdown format because it will be shared in a text-only terminal interface."
+
+	}
+
+	return prompt
+}
+
+// help - urfave/cli
 func main() {
+
+	var prompt string
+
+	cmd := &cli.Command{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "prompt",
+				Value:       "commit",
+				Destination: &prompt,
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			if prompt == "review" {
+				prompt = getPrompt(PromptCodeReview)
+			} else {
+				prompt = getPrompt(PromptCommitMessage)
+			}
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	// Run the git diff command
 	err, stdout, stderr := runDiff()
 	if err != nil {
@@ -45,7 +95,6 @@ func main() {
 		return
 	}
 
-	prompt := "please generate a git commit message with a simple explanation from the changes stated above which is an output of a git diff command. all response of this message should be wrapped in a markdown format because it will be shared in a text-only terminal interface."
 	result := fmt.Sprintf("~~~diff\n%s~~~\n\n%s", stdout, prompt)
 
 	// Print the output
@@ -57,32 +106,46 @@ func main() {
 		return
 	}
 
-	client := resty.New()
-
-	response, err := client.R().
-		SetAuthToken(apiKey).
-		SetHeader("Content-Type", "application/json").
-		SetBody(map[string]interface{}{
-			"model":      "gpt-4o",
-			"messages":   []interface{}{map[string]interface{}{"role": "system", "content": result}},
-			"max_tokens": 50,
-		}).
-		Post(apiEndpoint)
-
+	// Create the request body
+	body, err := json.Marshal(map[string]interface{}{
+		"model":      "gpt-4o",
+		"messages":   []interface{}{map[string]interface{}{"role": "system", "content": result}},
+		"max_tokens": 3500,
+	})
 	if err != nil {
-		log.Fatalf("Error while sending send the request: %v", err)
+		log.Fatalf("Error while marshalling request body: %v", err)
+		return
 	}
 
-	body := response.Body()
+	// Create the HTTP request
+	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	// Send the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error while sending request: %v", err)
+		return
+	}
+	defer res.Body.Close()
 
 	var data map[string]interface{}
-	err = json.Unmarshal(body, &data)
+
+	err = json.NewDecoder(res.Body).Decode(&data)
 	if err != nil {
 		fmt.Println("Error while decoding JSON response:", err)
 		return
 	}
 
 	// Extract the content from the JSON response
-	content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+	content := data
 	fmt.Println(content)
 }
+
